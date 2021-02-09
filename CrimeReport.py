96456fdf8fc5c2b4a41861e98023c2322cd0c3ef
jacobjwoods <jacobjwoods05@gmail.com>
2021-02-09 15:40:55 -0500

    added necessary files to implement search characteristics

---
+++ CrimeReport.py
@@ -0,0 +1,17 @@
+class CrimeReport:
+    def __init__(
+            self,
+            crimeID,
+            reportDate,
+            crimeStart,
+            crimeEnd,
+            offenses,
+            location,
+            disposition):
+        self.crimeID = crimeID
+        self.reportDate = reportDate
+        self.crimeStart = crimeStart
+        self.crimeEnd = crimeEnd
+        self.offenses = offenses
+        self.location = location
+        self.disposition = disposition
