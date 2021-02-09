15da6785e4b857f301caa51dcc0362a191caddf2
Neil <neilmckibben@gmail.com>
2021-02-08 22:04:54 -0500

    Write new crime reports to database, removed constant values

--- database.py
+++ database.py
@@ -1,12 +1,29 @@
 import pymongo
 import pprint
 from pymongo import MongoClient
+from pymongo import UpdateOne
+import Constants
+from WatsonInterface import WatsonInterface
+from bson.objectid import ObjectId
+
 #Starter code to get the connection going
-PASSWORD = "H0g*$aP0f5CR"
-DATABASE_NAME = "CrimeDatabase"
-MONGO_DB_URI = "mongodb+srv://neilmckibben:" + PASSWORD + "@crimedatabase.3hugk.mongodb.net/" + DATABASE_NAME + "?retryWrites=true&w=majority"
-client = MongoClient(MONGO_DB_URI)
-db = client.CrimeDatabase
-collection = db.OSUCrimeReport
-for a in collection.find():
-    print(a)
+class Database:
+    client = MongoClient(Constants.MONGO_DB_URI)
+    db = client.CrimeDatabase
+    collection = db.OSUCrimeReport
+
+    def processCrimeReport(self):
+        interface = WatsonInterface()
+        crime_reports = interface.createCrimeListObjects()
+        requests = [UpdateOne({'crimeID' : crime.crimeID},
+                    {'$set': {'reportDate' : crime.reportDate, 'crimeStart' : crime.crimeStart,
+                    'crimeEnd' : crime.crimeEnd, 'offenses' : crime.offenses,
+                    'location' : crime.location, 'disposition' : crime.disposition}}, upsert = True)
+                     for crime in crime_reports]
+        print("Inserting or updating up to ", len(requests), " items...")
+        self.collection.bulk_write(requests)
+        print("Operation complete")
+
+    #Only use if you want to delete ALL of the items in the database
+    def delete(self):
+        self.collection.remove()
