5cc2bc2ca692390f5d149a5bba5b495520498a99
Neil <neilmckibben@gmail.com>
2021-02-11 17:42:16 -0500

    Added table with MongoDB population

--- main.py
+++ main.py
@@ -1,7 +1,58 @@
 from flask import Flask
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import pandas as pd
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_table
+from dash.dependencies import Input, Output
+from database import Database
+
+
 app = Flask(__name__)
-
-
-@app.route('/')
+server = Flask(__name__)
+db = Database()
+df = db.getTableData()
+app = dash.Dash(
+    __name__,
+    server=server,
+    url_base_pathname='/'
+)
+app.layout = html.Div(id='dash-container')
+app.layout = html.Div([
+    html.H1(id='header', children='Test'),
+    html.Hr(className='solid'),
+    html.Br(),
+    dcc.Dropdown(
+        id='dropdown-year',
+        # options=[{'label': year, 'value': year} for year in years],
+        value='',
+        multi=True
+    ),
+    html.Br(),
+    html.Br(),
+    dash_table.DataTable(
+        id='datatable-interactivity',
+        columns=[{"name": i, "id": i} for i in df.columns],
+        data=df.to_dict('records'),
+        # callback info
+        editable=True,
+        filter_action="native",
+        sort_action="native",
+        sort_mode="multi",
+        column_selectable="single",
+        row_selectable="multi",
+        row_deletable=True,
+        selected_columns=[],
+        selected_rows=[],
+        page_action="native",
+        page_current=0,
+        page_size=10,
+    ),
+    html.Div(id='datatable-interactivity-container')
+])
+@server.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return app.index()
