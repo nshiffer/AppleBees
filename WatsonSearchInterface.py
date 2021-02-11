366eb4a286a73de025c3a7f12a217c71883b7439
jacobjwoods <jacobjwoods05@gmail.com>
2021-02-10 21:45:25 -0500

    Add catch for header being added to query

--- WatsonSearchInterface.py
+++ WatsonSearchInterface.py
@@ -33,59 +33,65 @@ class WatsonSearchInterface:
         results = self.querySearch(search)
         for query in results:
             for i in range(len(query["results"])):
-                print(f'Crime Number: {query["results"][i]["title"][0]}')
+                try:
+                    print(i)
+                    print(f'Crime Number: {query["results"][i]["title"][0]}')
 
-                # Finds dates and returns list
-                # List is in order of [Date/Time reported, Date/Time Started, Date/Time Ended]
-                # Crime is not guaranteed to have a Date/Time Ended
-                dates = re.findall(
-                    "\\d{2}/\\d{2}/\\d{2} \\d{2}:\\d{2}",
-                    query["results"][i]["text"])
-                print(f'Dates: {dates}')
+                    # Finds dates and returns list
+                    # List is in order of [Date/Time reported, Date/Time Started, Date/Time Ended]
+                    # Crime is not guaranteed to have a Date/Time Ended
+                    dates = re.findall(
+                        "\\d{2}/\\d{2}/\\d{2} \\d{2}:\\d{2}",
+                        query["results"][i]["question"][0])
+                    print(f'Dates: {dates}')
 
-                # Some crimes have multiple offenses which are usually separated by ';' character
-                # This is not guaranteed as formatting is a bit different for
-                # some crime reports
-                offenseList = (query["results"][i]["question"][0]).split(';')
-                print(f'Offense: {offenseList}')
+                    # Some crimes have multiple offenses which are usually separated by ';' character
+                    # This is not guaranteed as formatting is a bit different for
+                    # some crime reports
+                    offenseList = (query["results"][i]["text"]).split(';')
+                    print(f'Offense: {offenseList}')
 
-                print(f'Location: {query["results"][i]["subtitle"][0]}')
+                    print(f'Location: {query["results"][i]["subtitle"][0]}')
 
-                print(f'Disposition: {query["results"][i]["author"][0]}\n')
+                    print(f'Disposition: {query["results"][i]["author"][0]}\n')
+                except KeyError:
+                    print("------------FUCK YOU DISCOVERY-------------")
 
     def createCrimeListObjects(self, search):
         results = self.querySearch(search)
         crimeObjects = []
         for query in results:
             for i in range(len(query["results"])):
-                crimeID = query["results"][i]["title"][0]
-                # Finds dates and returns list
-                # List is in order of [Date/Time reported, Date/Time Started, Date/Time Ended]
-                # Crime is not guaranteed to have a Date/Time Ended
-                dates = re.findall(
-                    "\\d{2}/\\d{2}/\\d{2} \\d{2}:\\d{2}",
-                    query["results"][i]["text"])
-                date_format_string = '%m/%d/%y %H:%M'
-                reportDate = datetime.datetime.strptime(
-                    dates[0], date_format_string)
-                crimeStart = datetime.datetime.strptime(
-                    dates[1], date_format_string)
-                crimeEnd = None if len(dates) < 3 else datetime.datetime.strptime(
-                    dates[2], date_format_string)
-                # Some crimes have multiple offenses which are usually separated by ';' character
-                # This is not guaranteed as formatting is a bit different for
-                # some crime reports
-                offenseList = (query["results"][i]["question"][0]).split(';')
-                location = query["results"][i]["subtitle"][0]
-                disposition = query["results"][i]["author"][0]
-                crime = CrimeReport(
-                    crimeID,
-                    reportDate,
-                    crimeStart,
-                    crimeEnd,
-                    offenseList,
-                    location,
-                    disposition)
-                crimeObjects.append(crime)
-
+                try:
+                    crimeID = query["results"][i]["title"][0]
+                    # Finds dates and returns list
+                    # List is in order of [Date/Time reported, Date/Time Started, Date/Time Ended]
+                    # Crime is not guaranteed to have a Date/Time Ended
+                    dates = re.findall(
+                        "\\d{2}/\\d{2}/\\d{2} \\d{2}:\\d{2}",
+                        query["results"][i]["question"])
+                    date_format_string = '%m/%d/%y %H:%M'
+                    reportDate = datetime.datetime.strptime(
+                        dates[0], date_format_string)
+                    crimeStart = datetime.datetime.strptime(
+                        dates[1], date_format_string)
+                    crimeEnd = None if len(dates) < 3 else datetime.datetime.strptime(
+                        dates[2], date_format_string)
+                    # Some crimes have multiple offenses which are usually separated by ';' character
+                    # This is not guaranteed as formatting is a bit different for
+                    # some crime reports
+                    offenseList = (query["results"][i]["text"][0]).split(';')
+                    location = query["results"][i]["subtitle"][0]
+                    disposition = query["results"][i]["author"][0]
+                    crime = CrimeReport(
+                        crimeID,
+                        reportDate,
+                        crimeStart,
+                        crimeEnd,
+                        offenseList,
+                        location,
+                        disposition)
+                    crimeObjects.append(crime)
+                except KeyError:
+                    print("------------FUCK YOU DISCOVERY-------------")
         return crimeObjects
