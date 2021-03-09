8492160fd93d8b4623ce9edc858eb56f48186d43
Luis Morales <32255268+1MoralS@users.noreply.github.com>
2021-03-09 18:23:46 -0500

    Made changes to add nav bar

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+from flask_nav import Nav
+from flask_nav.elements import *
 
+app = Flask(__name__)
+# registers the "top" menubar
+nav = Nav(app)
+
+#here we define our menu items
+topbar = Navbar('thenav', View('Home Page', 'index'))
+nav.register_element('nav_bar', topbar)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html')
